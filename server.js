6e2d3e4e0378f9ff9e250c06ee7d3fdabbe6ce9e
sarkamchit <78621497+sarkamchit@users.noreply.github.com>
2021-04-04 22:13:53 +0400

    Update server.js

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ io.on("connection", socket => {
 if (process.env.PROD){
 app.use(express.static((path.john(__dirname, './client/build')));
 app.get('*', (req, res) => {
-    res.sendFile(path.john(__dirname, './client/build/index.html'));
+    res.sendFile(path.join(__dirname, './client/build/index.html'));
 });
         
 }
